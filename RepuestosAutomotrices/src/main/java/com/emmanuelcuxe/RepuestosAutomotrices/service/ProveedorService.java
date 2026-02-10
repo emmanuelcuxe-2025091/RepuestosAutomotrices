b5f49fc228a49483d3cafa03c059988bc592fc4d
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-02-10 11:49:18 -0600

    feat: Se agrego el codigo de la clase ProveedorService

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/ProveedorService.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/ProveedorService.java
@@ -1,4 +1,15 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.service;
 
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Proveedor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ProveedorService {
+    List<Proveedor> getAllProveedor();
+    Proveedor getProveedorById(Integer id);
+    Proveedor saveProveedor (Proveedor proveedor) throws RuntimeException;
+    Proveedor updateProveedor (Integer id, Proveedor proveedor);
+    void deleteProveedor (Integer id);
 }
