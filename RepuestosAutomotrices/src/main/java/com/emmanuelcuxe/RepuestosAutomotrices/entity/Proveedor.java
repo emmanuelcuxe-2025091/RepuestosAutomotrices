8a950c3c8e4ad9322bb219c073d2dca6159c11a7
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-02-10 11:28:38 -0600

    feat: Se agrego el codigo de la clase Proveedor

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/entity/Proveedor.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/entity/Proveedor.java
@@ -1,14 +1,46 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
 
 @Entity
 @Table (name = "Proveedores")
 
 public class Proveedor {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTIFY)
     @Column (name = "id_proveedor")
     private Integer idProveedor;
 
-    
+    @Column (name = "nombre_proveedor")
+    private String nombreProveedor;
+
+    @Column (name = "telefono_proveedor")
+    private Integer telefonoProveedor;
+
+    @Column (name = "direccion")
+    private String direccionProveedor;
+
+    @Column (name = "email_proveedor")
+    private String emailProveedor;
+
+    public Integer getIdProveedor() { return idProveedor; }
+
+    public void setIdProveedor(Integer idProveedor) { this.idProveedor = idProveedor; }
+
+    public String getNombreProveedor() { return nombreProveedor; }
+
+    public void setNombreProveedor(String nombreProveedor) { this.nombreProveedor = nombreProveedor; }
+
+    public Integer getTelefonoProveedor() { return telefonoProveedor; }
+
+    public void setTelefonoProveedor(Integer telefonoProveedor) { this.telefonoProveedor = telefonoProveedor; }
+
+    public String getDireccionProveedor() { return direccionProveedor; }
+
+    public void setDireccionProveedor(String direccionProveedor) { this.direccionProveedor = direccionProveedor; }
+
+    public String getEmailProveedor() { return emailProveedor; }
+
+    public void setEmailProveedor(String emailProveedor) { this.emailProveedor = emailProveedor; }
 }
