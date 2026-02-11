2d9cd79e85cd18913624b1d2c3dbd003a21c836d
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-02-10 20:32:44 -0600

    feat: Se agrego el codigo de la clase ProveedorController

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/controller/ProveedorController.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/controller/ProveedorController.java
@@ -1,12 +1,51 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.controller;
 
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Proveedor;
+import com.emmanuelcuxe.RepuestosAutomotrices.service.ProveedorService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.emmanuelcuxe.RepuestosAutomotrices.service.EmpleadoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping ("api/proveedores")
-public class ProveedoresController {
-    private final ProveedorService empleadoService;
+public class ProveedorController {
+    private final ProveedorService proveedorService;
+
+    public ProveedorController(ProveedorService proveedorService) { this.proveedorService = proveedorService; }
+
+    @GetMapping
+    public List<Proveedor> getAllProveedor() { return proveedorService.getAllProveedor(); }
+
+    @PostMapping
+    public ResponseEntity<Object> createProveedor(@Valid @RequestBody Proveedor proveedor) {
+        try {
+            Proveedor createdProveedor = proveedorService.saveProveedor(proveedor);
+            return new ResponseEntity<>(createdProveedor, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProveedor(@PathVariable Integer id, @Valid @RequestBody Proveedor proveedor) {
+        try {
+            Proveedor updateProveedor = proveedorService.updateProveedor(id, proveedor);
+            return new ResponseEntity<>(updateProveedor, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProveedor(@Valid @PathVariable Integer id) {
+        try {
+            proveedorService.deleteProveedor(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
