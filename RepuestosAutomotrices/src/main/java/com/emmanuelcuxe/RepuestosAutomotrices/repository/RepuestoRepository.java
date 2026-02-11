8e744f3edf3cfabf2166398b2bf794bc805224c0
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-02-11 10:56:54 -0600

    add: Se agrego la clase RepuestoRepository

---
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/repository/RepuestoRepository.java
@@ -0,0 +1,10 @@
+package com.emmanuelcuxe.RepuestosAutomotrices.repository;
+
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Repuesto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
+
+}
