38efd63920c34a28f6e5806c16f0c645fb991641
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-02-11 11:16:01 -0600

    feat: Se agrego el codigo de la clase Repuesto

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/entity/Repuesto.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/entity/Repuesto.java
@@ -1,4 +1,52 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Repuestos")
+
 public class Repuesto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    @Column(name = "nombre_repuesto")
+    private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private String precioCompra;
+
+    @Column(name = "precio_venta")
+    private String precioVenta;
+
+    @Column(name = "id_proveedor")
+    private String idProveedor;
+
+    public Integer getIdRepuesto() { return idRepuesto; }
+
+    public void setIdRepuesto(Integer idRepuesto) { this.idRepuesto = idRepuesto; }
+
+    public String getNombreRepuesto() { return nombreRepuesto; }
+
+    public void setNombreRepuesto(String nombreRepuesto) { this.nombreRepuesto = nombreRepuesto; }
+
+    public String getCategoriaRepuesto() { return categoriaRepuesto; }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) { this.categoriaRepuesto = categoriaRepuesto; }
+
+    public String getPrecioCompra() { return precioCompra; }
+
+    public void setPrecioCompra(String precioCompra) { this.precioCompra = precioCompra; }
+
+    public String getPrecioVenta() { return precioVenta; }
+
+    public void setPrecioVenta(String precioVenta) { this.precioVenta = precioVenta; }
+
+    public String getIdProveedor() { return idProveedor; }
+
+    public void setIdProveedor(String idProveedor) { this.idProveedor = idProveedor; }
 }
